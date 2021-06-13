46c6bc10654fe43f0e0cbdc5ef5ccfe6bdcd4ffe
zerowu49 <jonathanputra67@gmail.com>
2021-06-13 21:00:31 +0700

    add: auth api

---
+++ src/services/postgres/AuthenticationsService.js
@@ -0,0 +1,49 @@
+const {
+    nanoid
+} = require('nanoid');
+const bcrypt = require('bcrypt');
+const {
+    Pool
+} = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class AuthenticationsService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    async addRefreshToken(token) {
+        const query = {
+            text: 'INSERT INTO authentications VALUES($1)',
+            values: [token],
+        };
+
+        await this._pool.query(query);
+    }
+
+    async verifyRefreshToken(token) {
+        const query = {
+            text: 'SELECT token FROM authentications WHERE token = $1',
+            values: [token],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new InvariantError('Refresh token tidak valid');
+        }
+    }
+
+    async deleteRefreshToken(token) {
+        await this.verifyRefreshToken(token);
+
+        const query = {
+            text: 'DELETE FROM authentications WHERE token = $1',
+            values: [token],
+        };
+        await this._pool.query(query);
+    }
+}
+
+module.exports = AuthenticationsService;
